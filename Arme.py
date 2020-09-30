35715acb73c4add139f40559c14c14426a7b3a08
Sofiane ZOUAOUI <sofiane050900@gmail.com>
2020-09-30 02:11:08 +0200

    sous class Item

---
+++ Arme.py
@@ -0,0 +1,47 @@
+from Item import *
+from random import randint
+
+class Arme(Item):
+
+    def __init__(self,name="Item_Arme",height=0.1,quantity=1,price=0,nb_d=3,val_d=5,scope=3):
+        super().__init__(name,height,quantity,price)
+        self.nb_d = nb_d
+        self.val_d =val_d
+        self.scope = scope      
+
+
+
+
+    def attack(self):
+        dmg = 0
+        for i in range(self.getNb_d()):
+            dmg += randint(1,self.getVal_d())
+        
+        return dmg
+    
+
+
+
+
+    #####SETTER######
+
+    def setNb_d(self,nb_d):
+        self.nb_d = nb_d 
+
+    def setVal_d(self,val_d):
+        self.val_d = val_d 
+
+    def setScope(self,scope):
+        self.scope = scope
+
+
+    ######GETTER#####
+
+    def getNb_d(self):
+        return self.nb_d
+
+    def getVal_d(self):
+        return self.val_d
+
+    def getScope(self):
+        return self.scope
