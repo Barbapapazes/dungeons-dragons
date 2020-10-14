2fb374ee6611931f6bf6da543976dfc99b7f08b0
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-10-15 01:15:05 +0200

    feat(config): add global var

---
+++ config/colors.py
@@ -0,0 +1,11 @@
+"""Define colors"""
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BROWN = (106, 55, 5)
+CYAN = (0, 255, 255)
---
+++ config/window.py
@@ -0,0 +1,6 @@
+"""Define settings for the window"""
+
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+TITLE = 'Donjons et Dragons'
