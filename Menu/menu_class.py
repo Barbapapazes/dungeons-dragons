eedb3f8628d3cc4506bde6430b950637576248b0
AlysonAoustin <alyson.aoustin@insa-cvl.fr>
2020-11-02 15:02:03 +0100

    Réorganisation

--- Menu/menu_class.py
+++ Menu/menu_class.py
@@ -1,6 +1,8 @@
 import pygame
 import pygame_widgets as pw
 
+
+
 pygame.init()
 police=pygame.font.Font("Pixeled.ttf",40)
 police2=pygame.font.Font("Sindentosa.ttf",40)
@@ -8,6 +10,9 @@ police_small=pygame.font.Font("Pixeled.ttf",20)
 
 HAUTEUR_BOUTON=50
 LARGEUR_BOUTON=350
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
 
 surface= pygame.display.set_mode((1024,768))
 class classe_Menu_prin():
@@ -75,3 +80,48 @@ class classe_Menu_prin():
 
             self.Quiter.listen(event)
             self.Quiter.draw()
+
+
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
