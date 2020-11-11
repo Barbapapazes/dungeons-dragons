0444fd80ae71051dc1755459e280514263443170
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-11-11 22:15:29 +0100

    feat: logger add colorlog to logger

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 """Logger"""
 
 import logging
+from colorlog import ColoredFormatter
 
 # Create a custom logger
 logger = logging.getLogger(__name__)
@@ -12,7 +13,15 @@ c_handler.setLevel(logging.DEBUG)
 # f_handler.setLevel(logging.ERROR)
 
 # Create formatters and add it to handlers
-c_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+c_format = ColoredFormatter(
+    '%(asctime)s - %(log_color)s%(levelname)s%(reset)s - %(message)s',
+    log_colors={
+        'DEBUG':    'cyan',
+        'INFO':     'green',
+        'WARNING':  'yellow',
+        'ERROR':    'red',
+        'CRITICAL': 'red,bg_white',
+    })
 # f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 c_handler.setFormatter(c_format)
 # f_handler.setFormatter(f_format)
