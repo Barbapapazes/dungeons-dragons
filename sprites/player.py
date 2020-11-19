a2e08a836c8b77cf165340dfab63d04a76ea8a4b
YouKuulele <ramadi.younes@gmail.com>
2020-11-19 17:45:52 +0100

    Inventaire fonctionnel

--- sprites/player.py
+++ sprites/player.py
@@ -3,6 +3,7 @@ import pygame as pg
 from config.colors import YELLOW
 from config.window import TILESIZE
 from config.sprites import PLAYER_SPEED
+from inventory.inventory import Inventory
 
 
 class Player(pg.sprite.Sprite):
@@ -19,6 +20,17 @@ class Player(pg.sprite.Sprite):
         self.rect.x = self.x
         self.rect.y = self.y
 
+        #Stats
+        self.HP = 100       #point de vie
+        self.max_HP = 100
+        self.shield = 0     #armure
+
+        #Inventory
+        self.armor = {'head': None, 'chest': None, 'legs': None, 'feet': None}
+        self.weapon = None
+        self.inventory = Inventory(self, 40, 5, 8)
+
+
     def get_keys(self):
         self.vx, self.vy = 0, 0
         keys = pg.key.get_pressed()
@@ -33,6 +45,34 @@ class Player(pg.sprite.Sprite):
         if self.vx != 0 and self.vy != 0:
             self.vx *= 0.7071
             self.vy *= 0.7071
+
+    def addHp(self, hp_gain):
+        self.HP += hp_gain
+        if self.HP > self.max_HP:
+            self.HP = self.max_HP
+
+    def addShield(self, shield_gain):
+        self.shield += shield_gain
+
+    def equip_armor(self, item):
+        if self.armor[item.slot] != None:
+            self.unequip_armor(item.slot)
+        self.armor[item.slot] = item
+        self.shield += item.shield
+
+    def unequip_armor(self, slot):
+        if self.armor[slot] != None:
+            self.shield -= self.armor[slot].shield
+            self.armor[slot] = None
+
+    def equip_weapon(self, weapon):
+        if self.weapon != None:
+            self.unequip_weapon()
+        self.weapon = weapon
+
+    def unequip_weapon(self):
+        if self.weapon != None:
+            self.weapon = None
 
     def update(self):
         self.get_keys()
