12f329c773f5a3e049e15e8e147f9b063c936694
AlysonAoustin <alyson.aoustin@insa-cvl.fr>
2020-11-19 14:25:40 +0100

    BG MENU
    19/11

--- screens/menu.py
+++ screens/menu.py
@@ -2,6 +2,7 @@
 
 import pygame as pg
 import pygame_widgets as pw
+from os import path
 from window import _State
 from config.window import WIDTH, HEIGHT
 from config.screens import GAME, MENU, TRANSITION_OUT
@@ -22,19 +23,28 @@ class Menu(_State):
         self.next = GAME
         self.background = pg.Surface((WIDTH, HEIGHT))
         self.startup(0, 0)
+
+        #Backgroud image 
+        self.image=pg.image.load("./img/Menu/background.jpg").convert()
+        self.image=pg.transform.scale(self.image,(WIDTH,HEIGHT))
+
+
+
+
+        #Buttons 
         self.New_Game = pw.Button(
                 self.background, 337, 250, LARGEUR_BOUTON, HAUTEUR_BOUTON, text='New Game',
                 fontSize=50, margin=20,
                 inactiveColour=(13, 71, 32),
                 hoverColour=(9,48,22),
                 pressedColour=(9, 48, 22), radius=20,
-                onClick=self.fonction,
+                onClick=self.Change_state,
                 textVAlign="centre",
                 textHAlign="centre"
             )
         
-    def fonction(self):
-        print("Je change d'etat")
+    def Change_state(self):
+        #changement etat
         super().set_state(TRANSITION_OUT)
 
 
@@ -44,6 +54,7 @@ class Menu(_State):
         self.dt = dt
         self.background.fill((0, 255, 0))
         pg.init()
+        pg.display.set_mode((WIDTH,HEIGHT))
         super().setup_transition()
 
     def run(self, surface, keys, mouse, dt):
@@ -77,6 +88,7 @@ class Menu(_State):
     def draw(self):
         """Draw content"""
         self.screen.blit(self.background, (0, 0))
+        self.background.blit(self.image,(0,0))
         self.New_Game.listen(pg.event.get())
         self.New_Game.draw()
         
