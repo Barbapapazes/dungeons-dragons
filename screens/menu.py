2bcc95443a1c54255b2df3e1016ba85ab843729f
AlysonAoustin <alyson.aoustin@insa-cvl.fr>
2020-11-19 13:56:28 +0100

    Ajout bouton menu+ pw in pipfile
    19/11/2020

--- screens/menu.py
+++ screens/menu.py
@@ -1,9 +1,16 @@
 """Menu screen"""
 
 import pygame as pg
+import pygame_widgets as pw
 from window import _State
 from config.window import WIDTH, HEIGHT
 from config.screens import GAME, MENU, TRANSITION_OUT
+
+
+HAUTEUR_BOUTON=50
+LARGEUR_BOUTON=350
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
 
 
 class Menu(_State):
@@ -13,16 +20,30 @@ class Menu(_State):
         self.name = MENU
         super(Menu, self).__init__(self.name)
         self.next = GAME
-
         self.background = pg.Surface((WIDTH, HEIGHT))
-
         self.startup(0, 0)
+        self.New_Game = pw.Button(
+                self.background, 337, 250, LARGEUR_BOUTON, HAUTEUR_BOUTON, text='New Game',
+                fontSize=50, margin=20,
+                inactiveColour=(13, 71, 32),
+                hoverColour=(9,48,22),
+                pressedColour=(9, 48, 22), radius=20,
+                onClick=self.fonction,
+                textVAlign="centre",
+                textHAlign="centre"
+            )
+        
+    def fonction(self):
+        print("Je change d'etat")
+        super().set_state(TRANSITION_OUT)
+
 
     def startup(self, dt, game_data):
         """Initialize data at scene start."""
         self.game_data = game_data
         self.dt = dt
         self.background.fill((0, 255, 0))
+        pg.init()
         super().setup_transition()
 
     def run(self, surface, keys, mouse, dt):
@@ -48,6 +69,15 @@ class Menu(_State):
             if event.key == pg.K_p:
                 self.game_data['game_data']['count'] += 1
 
+
+    def affiche(self):
+        print('coucou')
+
+
     def draw(self):
         """Draw content"""
         self.screen.blit(self.background, (0, 0))
+        self.New_Game.listen(pg.event.get())
+        self.New_Game.draw()
+        
+   
