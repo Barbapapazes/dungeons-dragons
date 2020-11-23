e42c5393220c8453f7d8db20d50b52ed65ff0074
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-11-23 02:36:41 +0100

    feat(shortcuts): create a way to save new shortcuts

---
+++ data/shortcuts.py
@@ -0,0 +1,7 @@
+"""Dictionnary with all shortcuts"""
+
+
+SHORTCUTS_DEFAULT = {
+    "save a game": [False, False, 107],
+    "other": [False, True, 108]
+}
--- screens/shortcuts.py
+++ screens/shortcuts.py
@@ -1,10 +1,14 @@
 """Shortcuts screen"""
 
 import pygame as pg
+import json
+from os import path
 from window import _State
+from logger import logger
 from config.window import WIDTH, HEIGHT
-from config.colors import BLACK
+from config.colors import BLACK, WHITE
 from config.screens import SHORTCUTS, TRANSITION_OUT
+from data.shortcuts import SHORTCUTS_DEFAULT
 
 
 class Shortcuts(_State):
@@ -15,7 +19,18 @@ class Shortcuts(_State):
         super(Shortcuts, self).__init__(self.name)
         self.next = None
 
+        self.key = None
+
         self.background = pg.Surface((WIDTH, HEIGHT))
+
+        self.shortcuts = SHORTCUTS_DEFAULT
+        self.selected = 0
+        self.key = 107
+        self.ctrl = None
+        self.alt = None
+        self.saved = False
+        self.alpha = 0
+        logger.debug(self.shortcuts)
 
     def startup(self, dt, game_data):
         """Initialize data at scene start."""
@@ -40,7 +55,70 @@ class Shortcuts(_State):
 
     def get_events(self, event):
         """Events loop"""
+        if event.key is not pg.K_RETURN:
+            mod = pg.key.get_mods()
+            if mod & pg.KMOD_CTRL:
+                self.ctrl = not self.ctrl
+            elif mod & pg.KMOD_ALT:
+                self.alt = not self.alt
+            else:
+                self.key = event.key
+
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_PAGEUP:
+                self.selected += 1
+                if self.selected > len(self.shortcuts.keys()) - 1:
+                    self.selected = len(self.shortcuts.keys()) - 1
+            elif event.key == pg.K_PAGEDOWN:
+                self.selected -= 1
+                if self.selected < 0:
+                    self.selected = 0
+            elif event.key == pg.K_s and pg.key.get_mods() & pg.KMOD_ALT:
+                self.save_shortcuts()
+                self.saved = True
+            elif event.key == pg.K_RETURN:
+                for index, (key, [ctrl, alt, k_key]) in enumerate(self.shortcuts.items()):
+                    if index == self.selected:
+                        self.shortcuts[key] = self.create_shortcut()
+
+            logger.debug(self.selected)
+
+    def save_shortcuts(self):
+        """Save shortcuts to a custom file"""
+        with open(path.join(self.saved_shortcuts, 'custom_shortcuts.json'), 'w') as _f:
+            _f.write(json.dumps(self.shortcuts))
+            logger.info('Save shortcuts to %s', path.join(self.saved_shortcuts, 'custom_shortcuts.json'))
+
+    def create_shortcut(self):
+        return [self.ctrl, self.alt, self.key]
+
+    def create_text_shortcut(self, ctrl, alt, key):
+        return f"{'ctrl + ' if ctrl else ''} {'alt + ' if alt else ''} {pg.key.name(key) if key else ''}"
 
     def draw(self):
         self.screen.blit(self.background, (0, 0))
         self.draw_text("shortcuts".upper(), self.title_font, 48, BLACK, WIDTH // 2, 0, align="n")
+        self.draw_text(self.create_text_shortcut(self.ctrl, self.alt, self.key), self.title_font,
+                       36, BLACK, WIDTH // 2, HEIGHT // 4, align="n")
+        for index, (key, [ctrl, alt, k_key]) in enumerate(self.shortcuts.items()):
+            if index == self.selected:
+                self.draw_text(f"{key} : {self.create_text_shortcut(ctrl, alt, k_key)}", self.title_font,
+                               36, BLACK, WIDTH // 2, HEIGHT // 2, align="center")
+
+        self.draw_saved()
+
+    def draw_saved(self):
+        """Draw the saved page"""
+        if self.saved:
+            self.alpha += 15
+        if self.alpha >= 255:
+            self.saved = False
+        if self.alpha > 0 and not self.saved:
+            self.alpha -= 15
+        if self.saved or self.alpha > 0:
+            transition = pg.Surface((WIDTH, HEIGHT))
+            transition.fill(BLACK)
+            transition.set_alpha(self.alpha)
+            self.screen.blit(transition, (0, 0))
+            self.draw_text("Saved", self.title_font, 50, WHITE,
+                           WIDTH // 2, HEIGHT / 2, align="center")
