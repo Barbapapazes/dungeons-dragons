4070a03a3edce3cdf49ac457e2327d94a9e47074
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-11-23 15:13:02 +0100

    fix(shortcuts): rework test function

--- utils/shortcuts.py
+++ utils/shortcuts.py
@@ -12,4 +12,11 @@ def key_for(keys, event):
         Boolean
         """
     mods = pg.key.get_mods()
-    return ((not keys[0] and not keys[1]) and event.key == keys[2]) or(keys[0] and mods & pg.KMOD_CTRL and event.key == keys[2]) or (keys[1] and mods & pg.KMOD_ALT and event.key == keys[2]) or ((keys[0] and mods & pg.KMOD_CTRL) and (keys[1] and mods & pg.KMOD_ALT) and event.key == keys[2])
+    key = bool(not mods & pg.KMOD_ALT and not keys[1] and not mods &
+               pg.KMOD_CTRL and not keys[0] and event.key == keys[2])
+    ctrl = bool(not mods & pg.KMOD_ALT and not keys[1] and mods & pg.KMOD_CTRL and keys[0] and event.key == keys[2])
+    alt = bool(mods & pg.KMOD_ALT and keys[1] and not mods & pg.KMOD_CTRL and not keys[0] and event.key == keys[2])
+    if bool(mods & pg.KMOD_ALT) and bool(mods & pg.KMOD_CTRL):
+        return event.key == keys[2] and keys[0] and keys[1]
+    else:
+        return key or ctrl or alt
