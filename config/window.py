131feb1b5236c72d4cb5929a102d60f7343f6745
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-12-07 18:49:24 +0100

    feat(screen): add default value

--- config/window.py
+++ config/window.py
@@ -3,7 +3,13 @@
 
 from data.options import CUSTOM_SETTINGS_FILENAME
 import json
+from logger import logger
 from os import path
+
+default_settings = {
+    "width": 1024,
+    "height": 768
+}
 
 
 def get(_type):
@@ -16,8 +22,13 @@ def get(_type):
         int | str
     """
     value = 0
-    with open(path.join('.', 'assets', 'saved_settings', CUSTOM_SETTINGS_FILENAME), 'r') as _f:
-        value = json.load(_f)[_type]
+    try:
+        with open(path.join('.', 'assets', 'saved_settings', CUSTOM_SETTINGS_FILENAME), 'r') as _f:
+            value = json.load(_f)[_type]
+    except:
+        value = default_settings[_type]
+
+    logger.info("Load %s : %s", _type, value)
     return value
 
 
