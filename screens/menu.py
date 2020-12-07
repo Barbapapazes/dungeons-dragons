b83639b72b1bde0f068132f008ceaf0198739ae0
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-12-07 12:10:00 +0100

    feat(menu): refactor using the new class

--- screens/menu.py
+++ screens/menu.py
@@ -1,35 +1,18 @@
 """Menu screen"""
 
-from os import path
 import pygame as pg
-from window import _State
-from logger import logger
-from config.window import WIDTH, HEIGHT
-from config.screens import GAME, MENU, OPTIONS, TRANSITION_OUT, CREDITS, LOAD_GAME, CHARACTER_CREATION
-from config.colors import YELLOW_LIGHT, BEIGE, GREEN_DARK
-from config.buttons import HEIGHT_BUTTON, WIDTH_BUTTON, RADIUS_BUTTON, MARGIN_BUTTON
+from window import _Elements
+from config.screens import GAME, MENU, OPTIONS, LOAD_GAME, CHARACTER_CREATION
 from utils.shortcuts import load_shortcuts
 
 
-class Menu(_State):
+class Menu(_Elements):
     """Menu screen"""
 
     def __init__(self):
         self.name = MENU
-        super(Menu, self).__init__(self.name)
         self.next = GAME
-
-        # Background image
-        image = pg.image.load(
-            path.join(
-                self.img_folder, 'menu',
-                "background.jpg")).convert()
-        self.image = pg.transform.scale(image, (WIDTH, HEIGHT))
-
-        # Buttons
-        self.fontsize = 50
-
-        self.create_buttons()
+        super(Menu, self).__init__(self.name, self.next, 'menu', 'background.jpg', self.create_buttons_dict())
 
         self.startup(0, load_shortcuts())
 
@@ -58,37 +41,6 @@ class Menu(_State):
             }
         }
 
-    def create_buttons(self):
-        """Create buttons"""
-        _x = WIDTH // 2 - WIDTH_BUTTON // 2
-        y_base = 250
-        self.btns = list()
-        logger.info("Create all buttons from menu")
-        for index, (_, value) in enumerate(
-                self.create_buttons_dict().items()):
-            self.btns.append(self.create_button(
-                self.image,
-                _x,
-                y_base + index * 100,
-                WIDTH_BUTTON,
-                HEIGHT_BUTTON,
-                value["text"],
-                self.button_font,
-                self.fontsize,
-                MARGIN_BUTTON,
-                RADIUS_BUTTON,
-                BEIGE,
-                YELLOW_LIGHT,
-                GREEN_DARK,
-                value["on_click"],
-                value["on_click_params"]
-            ))
-
-    def load_next_state(self, *next_state):
-        """Load the new state"""
-        self.next = next_state[0]
-        super().set_state(TRANSITION_OUT)
-
     def run(self, surface, keys, mouse, dt):
         """Run states"""
         self.screen = surface
@@ -102,40 +54,12 @@ class Menu(_State):
 
     def normal_run(self):
         """Run the normal state"""
-        self.events_buttons()
+        super().events_buttons()
         self.draw()
-
-    def events_buttons(self):
-        """Used to manage the event for buttons"""
-        events = pg.event.get()
-        for btn in self.btns:
-            btn.listen(events)
 
     def draw(self):
         """Draw content"""
-        self.draw_background()
-        self.draw_title()
-        self.draw_buttons()
-
-    def draw_background(self):
-        """Draw the background"""
-        self.screen.blit(self.image, (0, 0))
-
-    def draw_title(self):
-        """Draw the title"""
-        self.draw_text(
-            "Dungeons and Dragons",
-            self.title_font,
-            150,
-            YELLOW_LIGHT,
-            WIDTH // 2,
-            15,
-            align="n")
-
-    def draw_buttons(self):
-        """Draw all buttons"""
-        for btn in self.btns:
-            btn.draw()
+        super().draw_elements("Dungeons and Dragons")
 
     @staticmethod
     def stop_window():
