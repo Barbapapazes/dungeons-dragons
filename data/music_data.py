44d47e35331515b2b4aed33a9ec291f46cfba4bc
AlysonAoustin <aoustinalyson@gmail.com>
2021-01-07 07:57:54 +0100

    test musique

--- data/music_data.py
+++ data/music_data.py
@@ -1,12 +1,14 @@
 
 DATA_MUSIC = {
     "is_enable" : True,
-    "volume" :0.15,
+    "volume" :0.14,
     "current_playing":None,
     "piste":{
         "menu":"enchanted-forest-music.mp3",
         "options":"test.wav",
         "options_music":"test.wav",
+        "new_game":"donjon.mp3",
+        "choose_map":"enchanted-forest-music.mp3"
             },
 }
 
