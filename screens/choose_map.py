0762665ab97e36c7786dc93e406c8758ec478c3a
Sofiane ZOUAOUI <sofiane050900@gmail.com>
2021-01-10 18:16:07 +0100

    add: delete bouton for floder random map

--- screens/choose_map.py
+++ screens/choose_map.py
@@ -5,6 +5,8 @@ from config.window import HEIGHT, WIDTH
 import pygame as pg
 from window import _Elements
 from config.screens import BACKGROUND_MENU, CHARACTER_CREATION, CHOOSE_MAP, CREDITS, GAME, INTRODUCTION, MENU, NEW_GAME, OPTIONS, LOAD_GAME, TRANSITION_IN, TRANSITION_OUT
+from config.colors import BEIGE,YELLOW_LIGHT,GREEN_DARK
+from config.buttons import WIDTH_BUTTON, HEIGHT_BUTTON, MARGIN_BUTTON, RADIUS_BUTTON
 import os
 from os import path
 from logger import logger
@@ -28,6 +30,12 @@ class ChooseMap(_Elements):
         self.maps = [
             f for f in os.listdir(
                 self.levels_maps) if f.endswith('.tmx')]
+        
+        self.maps_generated = [
+            f for f in os.listdir(
+                self.random_maps) if f.endswith('.tmx')]
+        
+        
 
         super(ChooseMap, self).__init__(self.name, self.next, 'menu', '0.png', self.create_buttons_dict())
 
@@ -79,6 +87,82 @@ class ChooseMap(_Elements):
             }
         return btns_dict
 
+    def create_delete_buttons(self):
+        """Create delete btn"""
+        _x = WIDTH // 2 + WIDTH_BUTTON
+        y_base = 250
+        logger.info("Create delete buttons")
+        for index, (_, value) in enumerate(self.create_delete_buttons_dict().items()):
+            self.delete_btns.append(self.create_button(
+                self.image_screen,
+                _x,
+                y_base + index * 70,
+                50,
+                HEIGHT_BUTTON,
+                value["text"],
+                self.button_font,
+                self.fontsize,
+                MARGIN_BUTTON,
+                RADIUS_BUTTON,
+                BEIGE,
+                YELLOW_LIGHT,
+                GREEN_DARK,
+                value["on_click"],
+                value["on_click_params"]
+
+        ))
+    
+    def create_delete_buttons_dict(self):
+        """"Create dict for the delete button"""
+        btns_dict = dict()
+        for index, _ in enumerate(self.maps_generated):
+            btns_dict[index] = {
+                "text": "X",
+                "on_click": self.delete,
+                "on_click_params": [index],
+            }
+        return btns_dict
+
+    def delete(self, num):
+        """Delete a file using the num
+
+        Args:
+            num (int)
+        """
+        file_name = self.maps_generated[num]
+        file_path = path.join(self.random_maps, file_name)
+        if path.exists(file_path):
+            os.remove(file_path)
+            logger.info("Remove the file %s", file_path)
+        else:
+            logger.error("The file %s doesn't existe", file_path)
+
+        pg.event.wait()
+        self.refresh()
+    
+    def refresh(self):
+        """Refresh the list"""
+        self.image_screen = self.image.copy()
+        self.maps_generated = [
+            f for f in os.listdir(
+                self.random_maps) if f.endswith('.tmx')]
+
+        self.maps = self.maps_generated
+               
+
+
+
+        self.create_back_button(self.image_screen, self.toggle_sub_state, ['normal'])
+
+        self.btns_dict = self.create_maps_buttons_dict()
+        self.create_buttons(self.image_screen, offset=70, width=WIDTH_BUTTON + 50)
+        
+        
+        self.delete_btns = list()
+        self.create_delete_buttons()
+
+        logger.info("Refresh the info")
+
     def load(self, index):
         selected = self.maps[index]
         self.game_data["game_data"]["map"] = {
@@ -122,6 +206,8 @@ class ChooseMap(_Elements):
             self.type_maps = "saved_generated"
             self.btns = list()
             self.create_buttons(self.image_screen, width=WIDTH_BUTTON + 50)
+            self.delete_btns = list()
+            self.create_delete_buttons()
         else:
             self.btns_dict = self.create_buttons_dict()
             self.btns = list()
@@ -166,10 +252,23 @@ class ChooseMap(_Elements):
         self.back_btn.draw()
 
     def random_maps_run(self):
+        for btn in self.delete_btns:
+            btn.draw()
         self.screen.blit(self.image_screen, (0, 0))
         super().events_buttons(back=True)
         super().draw_title("Random Maps")
         self.back_btn.draw()
+        self.draw_text("Delete", self.text_font, 26, BEIGE, WIDTH // 2 + WIDTH_BUTTON + 25, 230, align="s")
+        self.events_delete_btns()
+
+        
+
+    def events_delete_btns(self):
+        """Listen for delete btns"""
+        events = pg.event.get()
+        for btn in self.delete_btns:
+            btn.listen(events)
+
 
     def draw(self):
         super().draw_elements("Choose a map", back=True)
