0803ca0da7a15c072d6c0ab80bdeb49c7aad58e6
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-01-18 23:54:50 +0100

    fix(character): update imge time

--- sprites/character.py
+++ sprites/character.py
@@ -61,7 +61,7 @@ class Character(pg.sprite.Sprite):
             self.hit_rect = hit_rect
             self.hit_rect.center = self.rect.center
 
-        self.frame_time = 180 / 1000
+        self.frame_time = 210 / 1000
         self.frame_timer = 0
 
     def update(self):
