cebf845236518d5df48580b98f2659dd53b668ae
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-01-20 09:35:58 +0100

    fix #44

---
+++ assets/img/tile_dungeons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tile_dungeons" tilewidth="32" tileheight="32" tilecount="72" columns="6">
+ <image source="tile_dungeons.png" width="192" height="384"/>
+</tileset>
