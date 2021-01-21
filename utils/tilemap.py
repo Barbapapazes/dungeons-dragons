ed7f372259397d92bfe10ad9436351850129d250
valent33 <v.porchet@outlook.fr>
2021-01-21 09:34:14 +0100

    skip ok & further équilibrage

--- utils/tilemap.py
+++ utils/tilemap.py
@@ -21,8 +21,10 @@ def collide_with_walls(sprite, group, dir):
                 sprite.pos.x = hits[0].rect.left - sprite.hit_rect.width / 2
             if hits[0].rect.centerx < sprite.hit_rect.centerx:
                 sprite.pos.x = hits[0].rect.right + sprite.hit_rect.width / 2
-            sprite.vel.x = 0
+            if hasattr(sprite, 'gold'):
+                sprite.vel.x = 0
             sprite.hit_rect.centerx = sprite.pos.x
+            return True
     if dir == 'y':
         hits = pg.sprite.spritecollide(sprite, group, False, collide_hit_rect)
         if hits:
@@ -30,8 +32,10 @@ def collide_with_walls(sprite, group, dir):
                 sprite.pos.y = hits[0].rect.top - sprite.hit_rect.height / 2
             if hits[0].rect.centery < sprite.hit_rect.centery:
                 sprite.pos.y = hits[0].rect.bottom + sprite.hit_rect.height / 2
-            sprite.vel.y = 0
+            if hasattr(sprite, 'gold'):
+                sprite.vel.y = 0
             sprite.hit_rect.centery = sprite.pos.y
+            return True
 
 
 def collide_hit_rect(one, two):
