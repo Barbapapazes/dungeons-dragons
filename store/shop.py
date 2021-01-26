c913f38d6949c56e3dba620234378ae83042291b
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-01-26 17:34:42 +0100

    fix(shop): remove green rect

--- store/shop.py
+++ store/shop.py
@@ -129,7 +129,7 @@ class ShopSlot(Container):
             image_x = image.get_width()
             image_y = image.get_height()
 
-            pg.draw.rect(screen, (0, 255, 0), pg.Rect(self.x + offset / 2, self.y + offset / 2, image_x, image_y), 1)
+            # pg.draw.rect(screen, (0, 255, 0), pg.Rect(self.x + offset / 2, self.y + offset / 2, image_x, image_y), 1)
 
             screen.blit(image, (self.x + offset / 2, self.y + offset / 2))
 
